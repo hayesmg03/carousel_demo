6aa35c21a24236c283b0fc8ba934db434c102dba
m3hayes <m3hayes.student.bridgew.edu>
2025-08-04 17:22:55 -0400

    cards change locations

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+const container = document.getElementById("container");
 const card1 = document.getElementById("LA-card");
 const card2 = document.getElementById("Yosemite-card");
 
 var cards = [card1, card2];
+card1.remove()
+card2.remove()
+
+for (const card of cards) {
+    container.appendChild(card);
+}
 
 const left = document.getElementById("left");
 
-cards[cards.length - 1] = cards.shift();
-console.log(cards);
+
+left.addEventListener("click", function (e) {
+    cards[cards.length - 1] = cards.shift();
+    container.appendChild(cards[0]);
+});
