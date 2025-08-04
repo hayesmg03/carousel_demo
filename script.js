9adcea59a0bc3a2a48cb5c0171b8335e7d098af0
m3hayes <m3hayes.student.bridgew.edu>
2025-08-04 17:48:51 -0400

    left arrow working

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const container = document.getElementById("container");
 const card1 = document.getElementById("LA-card");
 const card2 = document.getElementById("Yosemite-card");
+const card3 = document.getElementById("DV-card")
 
-var cards = [card1, card2];
-card1.remove()
-card2.remove()
+var cards = [card1, card2, card3];
+card1.remove();
+card2.remove();
+card3.remove();
 
 for (const card of cards) {
     container.appendChild(card);
 }
 
 const left = document.getElementById("left");
+const right = document.getElementById("right");
 
 
 left.addEventListener("click", function (e) {
     cards[cards.length - 1] = cards.shift();
-    container.appendChild(cards[0]);
+    for (const card of cards){
+        container.appendChild(card)
+    }
+    console.log(container);
+    console.log(cards);
 });
+
+right.addEventListener("click", function (e) {
+    cards[0] = cards.pop();
+    console.log(cards);
+})
