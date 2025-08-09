4faf3ac4372bafd622436537c648d2e81a9b60d2
m3hayes <m3hayes.student.bridgew.edu>
2025-08-08 21:04:25 -0400

    removed redundant js

--- script.js
+++ script.js
@@ -4,13 +4,6 @@ const card2 = document.getElementById("Yosemite-card");
 const card3 = document.getElementById("DV-card")
 
 var cards = [card1, card2, card3];
-card1.remove();
-card2.remove();
-card3.remove();
-
-for (const card of cards) {
-    container.appendChild(card);
-}
 
 const left = document.getElementById("left");
 const right = document.getElementById("right");
